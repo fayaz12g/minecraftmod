f76a06aae96271c63b5a3562c0e13ce89bcf40cd
Fayaz Shaikh <61674751+fayaz12g@users.noreply.github.com>
2024-04-07 23:11:43 -0400

    replace

--- src/main/java/one/fayaz/potatomod/item/ModItems.java
+++
@@ -1,8 +0,0 @@
-package one.fayaz.potatomod.item;
-
-import net.minecraft.world.item.Item;
-import net.minecraft.registries.DeferredRegister;
-
-public class ModItems {
-    public static final DeferredRegister
-}
